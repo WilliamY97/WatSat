b23a0fc79c7f44eae85b4c4c98eb5e9f6afd04f8
William Yang <yangwilliam92@gmail.com>
2016-01-19 15:57:43 -0500

    update
    animation change

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// $(document).ready(function(){    
+// })
+
 $(document).ready(function() {
     
+    $('.first').delay(1000).fadeIn(500);
+
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
     
@@ -10,7 +15,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window+100 > bottom_of_object ){
+            if( bottom_of_window > bottom_of_object ){
                 
                 $(this).animate({'opacity':'1'},500);
                     
